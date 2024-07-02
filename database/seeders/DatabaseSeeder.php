99b4964703d3feb2070f416243b9ba1a1cbaba96
Juan Manuel Lukaszewicz <juanmanuelluk@gmail.com>
2024-07-02 12:16:16 -0300

    método this->call  en el database seeder principal  para que estos se ejecuten todos juntos y no tener que hacer uno por vez.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            TypesTableSeeder::class,
+            ImagesTableSeeder::class,
+            CandiesTableSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
